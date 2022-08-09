55c0674a80bdfa66f1d744ce1adf68c6bc712f9f
Jamez Mayke <jamezmayke@gmail.com>
2022-08-09 09:19:41 -0300

    Instalação do axios, configuração de arquivo para o axios em: src/services/api.ts

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 
+import { createServer } from 'miragejs';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+createServer({
+  routes() {
+    this.namespace = "api";
+
+    this.get("/transactions", () => {
+      return [
+        {
+          title: "Desenvolvimento Web",
+          value: 12000,
+          category: "desenvolvimento web",
+          date: new Date("08-08-2022 23:16:00").toLocaleString("pt-BR", {
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit'
+          })
+        }
+      ]
+    })
+  }
+})
+
 root.render(
   <React.StrictMode>
     <App />
