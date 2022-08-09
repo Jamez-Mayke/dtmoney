d4e5ca43eaddbf5c1be021ae4d8229e684fb59a0
Jamez Mayke <jamezmayke@gmail.com>
2022-08-09 11:07:32 -0300

    removendo conteúdo do modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import Modal from 'react-modal';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 import { useState } from 'react';
+import { NewTransactionModal } from './components/NewTransactionModal';
 
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
@@ -19,12 +19,10 @@ export function App() {
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
 
-      <Modal
-          isOpen={isNewTransactionModalOpen}
-          onRequestClose={handleCloseNewTransactionModal}
-      >
-          <h2>Nova Transação</h2>
-      </Modal>
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
       <GlobalStyle />
     </>
   );
